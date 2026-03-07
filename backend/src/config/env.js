67ecdf7fb8ab4a982748ad386fbc870031285519
samyak <samyakmhrzn9841@gmail.com>
2026-03-07 21:03:56 +0545

    setup dockerfile and compose for backend

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,11 +6,11 @@ export const config = {
   port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   db: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "db",
     port: parseInt(process.env.DB_PORT || "5432"),
     name: process.env.DB_NAME || "incorpfirm",
     user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "",
+    password: process.env.DB_PASSWORD || "pwd",
   },
   api: {
     url: process.env.API_URL || "http://localhost:3000",
